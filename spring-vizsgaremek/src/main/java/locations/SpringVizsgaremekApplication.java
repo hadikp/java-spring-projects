306ba009945bd71ed391d55693e348cdb650a40e
hadikp <hp20001025@gmail.com>
2022-05-22 16:13:22 +0200

    vizsgaremek start

---
+++ spring-vizsgaremek/src/main/java/locations/SpringVizsgaremekApplication.java
@@ -0,0 +1,13 @@
+package locations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringVizsgaremekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringVizsgaremekApplication.class, args);
+	}
+
+}

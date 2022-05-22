d4a568326a0266fd84f3b0740e9cb623295824f8
hadikp <hp20001025@gmail.com>
2022-05-22 16:01:59 +0200

    start spring-solutions copy

---
+++ spring-solutions/src/main/java/locations/SpringSolutionsApplication.java
@@ -0,0 +1,13 @@
+package locations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSolutionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSolutionsApplication.class, args);
+	}
+
+}

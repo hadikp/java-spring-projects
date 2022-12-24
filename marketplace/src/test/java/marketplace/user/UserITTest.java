be7be9bc11a7e3b4e63c3b3291db313bc31c512b
hadikp <hp20001025@gmail.com>
2022-12-24 21:29:34 +0100

    User Product Comment test

--- marketplace/src/test/java/marketplace/user/UserITTest.java
+++ marketplace/src/test/java/marketplace/user/UserITTest.java
@@ -1,5 +1,6 @@
 package marketplace.user;
 
+import marketplace.comment.Comment;
 import marketplace.product.Product;
 import marketplace.product.Type;
 import org.junit.jupiter.api.Test;
@@ -13,14 +14,37 @@ class UserITTest {
     @Autowired
     UserRepository repository;
 
+    User gergo;
+    User peter;
+    Product book1;
+    Product book2;
+    Product radio;
+
     @Test
     void testCreate(){
-        User user = new User("Péter", "hadikp", "hadikp@gmail.com", "pass123", LocalDate.of(2022, 12, 23));
-        Product konyv = new Product("könyv", "Ez egy könyv", "src/images", 510, Type.BOOK);
-        Product radio = new Product("rádió", "Ez egy rádió", "src/images", 510, Type.RADIO);
-        user.addProduct(konyv);
-        user.addProduct(radio);
-        repository.save(user);
+        gergo = new User("Gergő", "kissg", "kissg@gmail.com", "pass123", LocalDate.of(2022, 12, 23));
+        peter = new User("Péter", "hadikp", "hadikp@gmail.com", "p345", LocalDate.of(2022, 12, 24));
+        book1 = new Product("könyv", "Ez egy könyv", "src/images", 510, Type.BOOK);
+        book2 = new Product("Másik könyv", "Ez a másik könyv", "src/images", 1510, Type.BOOK);
+        radio = new Product("rádió", "Ez egy rádió", "src/images", 5510, Type.RADIO);
+
+        Comment com1 = new Comment("com1 content", LocalDate.of(2022, 12, 11));
+        Comment com2 = new Comment("com2 content", LocalDate.of(2022, 12, 12));
+        Comment com3 = new Comment("com3 content", LocalDate.of(2022, 12, 13));
+
+        book1.addComment(com1);
+        book1.addComment(com2);
+        book2.addComment(com3);
+
+        gergo.addProduct(book1);
+        gergo.addProduct(radio);
+        peter.addProduct(book2);
+
+        gergo.addComment(com1);
+        gergo.addComment(com2);
+        peter.addComment(com3);
+        repository.save(gergo);
+        repository.save(peter);
     }
 
 }

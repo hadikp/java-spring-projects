d216c6979c017651aeaa48d1066bdae4ca9239ab
hadikp <hp20001025@gmail.com>
2022-12-27 20:36:36 +0100

    BlueSight application start

---
+++ bluesight/src/test/java/bluesight/BlueSightApplicationTests.java
@@ -0,0 +1,13 @@
+package bluesight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlueSightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

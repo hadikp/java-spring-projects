e0fd8f39eb4753e87358dc59b3a8d1aa8a6874cc
hadikp <hp20001025@gmail.com>
2022-12-27 21:10:11 +0100

    BlueSight-Jpa start

---
+++ bluesight-Jpa/src/main/java/bluesight/BluesightJpaApplication.java
@@ -0,0 +1,13 @@
+package bluesight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BluesightJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BluesightJpaApplication.class, args);
+	}
+
+}
---
+++ bluesight-Jpa/src/test/java/bluesight/BluesightJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package bluesight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BluesightJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

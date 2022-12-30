104eca0f40ff6c3d7cde082d122adfdf314d3de9
hadikp <hp20001025@gmail.com>
2022-12-30 21:29:22 +0100

    mariaDB database prepare

--- bluesight-Jpa/src/main/java/bluesight/BluesightJpaApplication.java
+++ bluesight-Jpa/src/main/java/bluesight/BluesightJpaApplication.java
@@ -1,13 +1,20 @@
 package bluesight;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BluesightJpaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BluesightJpaApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper createModelMapper(){
+		return new ModelMapper();
 	}
 
 }

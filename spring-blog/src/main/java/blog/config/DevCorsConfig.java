3f2185d6aaf6503601df24c8ebd91f96a74c1f73
hadikp <hp20001025@gmail.com>
2023-07-12 17:47:20 +0200

    add Cors config

---
+++ spring-blog/src/main/java/blog/config/DevCorsConfig.java
@@ -0,0 +1,16 @@
+package blog.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@Profile("development")
+public class DevCorsConfig implements WebMvcConfigurer {
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS");
+    }
+}

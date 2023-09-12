d5eba66585d5fa19a09195b2323219011492aafd
hadikp <hp20001025@gmail.com>
2023-09-12 14:34:56 +0200

    start project

---
+++ marketplace-mongo/src/main/java/marketplace/MarketplaceMongoApplication.java
@@ -0,0 +1,13 @@
+package marketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketplaceMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketplaceMongoApplication.class, args);
+	}
+
+}
---
+++ marketplace-mongo/src/test/java/marketplace/MarketplaceMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package marketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketplaceMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

62fca7c5d9ac66fae54174d6d818457c83979230
hadikp <hp20001025@gmail.com>
2023-10-09 19:07:57 +0200

    start project

---
+++ property-firebase/src/main/java/pf/PropertyFirebaseApplication.java
@@ -0,0 +1,13 @@
+package pf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertyFirebaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertyFirebaseApplication.class, args);
+	}
+
+}
---
+++ property-firebase/src/test/java/pf/PropertyFirebaseApplicationTests.java
@@ -0,0 +1,13 @@
+package pf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertyFirebaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

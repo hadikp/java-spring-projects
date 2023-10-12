0b49fe235474daaf08c2831ca489f913289d7076
hadikp <hp20001025@gmail.com>
2023-10-12 20:08:28 +0200

    file upload

---
+++ property/src/main/java/p/file/FileInfo.java
@@ -0,0 +1,15 @@
+package p.file;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class FileInfo {
+
+    private String name;
+    private String url;
+
+}

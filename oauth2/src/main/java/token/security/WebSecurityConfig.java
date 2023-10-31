1de4ea4211a023eafb7f19a3c08ac69fc34b030f
hadikp <hp20001025@gmail.com>
2023-10-31 20:58:00 +0100

    some modifications

--- oauth2/src/main/java/token/security/WebSecurityConfig.java
+++ oauth2/src/main/java/token/security/WebSecurityConfig.java
@@ -11,7 +11,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
@@ -27,6 +26,8 @@ import org.springframework.security.web.SecurityFilterChain;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -46,11 +47,11 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         return http.csrf(csrf -> csrf.disable())
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(withDefaults()))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .exceptionHandling(configurer -> configurer.authenticationEntryPoint(WebSecurityConfig::handleException))
                 .authorizeHttpRequests(auth ->auth.anyRequest().authenticated())
-                .httpBasic(Customizer.withDefaults())
+                .httpBasic(withDefaults())
                 .build();
     }
 

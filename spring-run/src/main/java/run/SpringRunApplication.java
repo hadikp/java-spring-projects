1f84c7fcae259f10c575a50af8d3022d0d314812
hadikp <hp20001025@gmail.com>
2025-04-06 19:41:36 +0200

    run-book start

---
+++ spring-run/src/main/java/run/SpringRunApplication.java
@@ -0,0 +1,13 @@
+package run;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRunApplication.class, args);
+	}
+
+}
---
+++ spring-run/src/test/java/run/SpringRunApplicationTests.java
@@ -0,0 +1,13 @@
+package run;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

8459444399298d47caef1a3b3979308557497a43
hadikp <hp20001025@gmail.com>
2025-06-02 22:08:10 +0200

    add cors

--- marketplace/src/main/java/marketplace/config/DevCorsConfig.java
+++ marketplace/src/main/java/marketplace/config/DevCorsConfig.java
@@ -10,7 +10,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class DevCorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS");
     }
 }

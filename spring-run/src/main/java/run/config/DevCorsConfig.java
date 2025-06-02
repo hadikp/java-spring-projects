22e1f613513fa9e126a88b24b35370873af69208
hadikp <hp20001025@gmail.com>
2025-06-02 22:22:31 +0200

    cors repair

--- spring-run/src/main/java/run/config/DevCorsConfig.java
+++ spring-run/src/main/java/run/config/DevCorsConfig.java
@@ -1,16 +1,15 @@
-package marketplace.config;
+package run.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @Profile("development")
-public class DevCorsConfig implements WebMvcConfigurer {
+public class DevCorsConfig {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS");
     }
 }

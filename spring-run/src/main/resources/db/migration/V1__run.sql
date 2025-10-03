be2a3e246d6d7e569378e18dd4c5eece48a14b86
hadikp <hp20001025@gmail.com>
2025-10-03 19:03:27 +0200

    add run, training data

--- spring-run/src/main/resources/db/migration/V1__run.sql
+++ spring-run/src/main/resources/db/migration/V1__run.sql
@@ -1,30 +1,15 @@
 create table runs(id bigint auto_increment PRIMARY KEY, type varchar(50), description varchar(200), km double,
  run_date date, training_id bigint);
-insert into runs(type, description, km, run_date, training_id) values ('futás', 'hosszú futás', 50, '2025-01-04', 1);
-insert into runs(type, description, km, run_date, training_id) values ('futás', 'hosszú futás', 50, '2025-01-12', 2);
-insert into runs(type, description, km, run_date, training_id) values ('futás', 'hosszú futás', 50, '2025-01-18', 3);
-insert into runs(type, description, km, run_date, training_id) values ('futás', 'hosszú futás', 50, '2025-01-26', 4);
-insert into runs(type, description, km, run_date, training_id) values ('futás', 'hosszú futás', 50, '2025-02-01', 5);
-insert into runs(type, description, km, run_date, training_id) values ('futás', 'hosszú futás', 50, '2025-02-09', 6);
-insert into runs(type, description, km, run_date, training_id) values ('futás', 'hosszú futás', 50, '2025-02-15', 7);
-insert into runs(type, description, km, run_date, training_id) values ('futás', 'rövid futás', 15, '2025-02-23', 8);
-insert into runs(type, description, km, run_date, training_id) values ('futás', 'rövid futás', 15, '2025-03-01', 10);
-insert into runs(type, description, km, run_date, training_id) values ('futás', 'futás az Úrhidai telekre és vissza', 24, '2025-04-24', 12);
-insert into runs(type, description, km, run_date, training_id) values ('futás', 'futás Budai úton', 13, '2025-05-07', 16);
-insert into runs(type, description, km, run_date, training_id) values ('futás', 'futás az Úrhidai telekre és vissza', 24, '2025-05-16', 19);
-insert into runs(type, description, km, run_date, training_id) values ('futás', 'futás az Úrhidai telekre és vissza', 24, '2025-05-20', 20);
-insert into runs(type, description, km, run_date, training_id) values ('futás', 'futás az Úrhidai telekre és vissza', 24, '2025-05-27', 21);
-insert into runs(type, description, km, run_date, training_id) values ('futás', 'futás Pákozd felé oda-vissza', 36, '2025-05-31', 22);
-insert into runs(type, description, km, run_date, training_id) values ('futás', 'hosszú futás megint a régi távon', 50, '2025-06-07', 26);
-insert into runs(type, description, km, run_date, training_id) values ('futás', 'futás a Pákozdi erdőig oda-vissza', 30, '2025-06-13', 29);
-insert into runs(type, description, km, run_date, training_id) values ('futás', 'futás a Pákozdi erdőig oda-vissza', 50, '2025-06-21', 32);
-insert into runs(type, description, km, run_date, training_id) values ('futás', 'indulás:4:28 Halesz:18p Kisfalud:56p M7:1ó06p Lótusz:1ó18p kanyarbüfé:1ó31p Agárd:2ó04p Velencefürdő:2ó29p Velence:2ó54p Spar:35p Katonai:8:45 Kisfalud:9:54 Óra:10:22', 50, '2025-06-28', 33);
-insert into runs(type, description, km, run_date, training_id) values ('futás', 'indulás:5:10 Agárd:2ó05p Velence:2ö56p Óra:11:10', 50, '2025-07-05', 34);
-insert into runs(type, description, km, run_date, training_id) values ('futás', 'hosszú futás', 50, '2025-07-12', 35);
-insert into runs(type, description, km, run_date, training_id) values ('futás', 'hosszú futás', 50, '2025-07-26', 36);
-insert into runs(type, description, km, run_date, training_id) values ('futás', 'indulás:4:00 Agárd:2ó10p Velence:3ó02p Óra:09:55', 50, '2025-08-02', 37);
-insert into runs(type, description, km, run_date, training_id) values ('futás', 'indulás:4:21 Halesz:19p Kisfalud:54p M7:12p Lótusz:25p kanyarbüfé:12p Agárd:2ó04p Velencefürdő:31p Velence:2ó52p Spar:30p Katonai:8:30 Kisfalud:9:34 Óra:10:07', 55, '2025-08-07', 38);
-insert into runs(type, description, km, run_date, training_id) values ('futás', 'indulás:3:42 Agárd:2ó13p Velence:3ó10p Óra:09:55', 50, '2025-08-16', 39);
+insert into runs(type, description, km, run_date, training_id) values ('futás', 'hosszú futás', 50, '2025-07-26', 247);
+insert into runs(type, description, km, run_date, training_id) values ('futás', 'indulás:4:00 Agárd:2ó10p Velence:3ó02p Óra:09:55', 50, '2025-08-02', 248);
+insert into runs(type, description, km, run_date, training_id) values ('futás', 'indulás:4:21 Halesz:19p Kisfalud:54p M7:12p Lótusz:25p kanyarbüfé:12p Agárd:2ó04p Velencefürdő:31p Velence:2ó52p Spar:30p Katonai:8:30 Kisfalud:9:34 Óra:10:07', 55, '2025-08-07', 249);
+insert into runs(type, description, km, run_date, training_id) values ('futás', 'indulás:3:42 Agárd:2ó13p Velence:3ó10p Óra:09:55', 50, '2025-08-16', 250);
+insert into runs(type, description, km, run_date, training_id) values ('futás', 'indulás:4:10 Agárd:2ó15p Velence:3ó10p Óra:10:20', 50, '2025-08-20', 251);
+insert into runs(type, description, km, run_date, training_id) values ('futás', 'indulás:4:10 Agárd:2ó20p Velence:3ó11p Óra:10:16', 51, '2025-08-30', 252);
+insert into runs(type, description, km, run_date, training_id) values ('futás', 'indulás:4:10 Agárd:2ó10p Velence:3ó11p Óra:10:16', 52, '2025-09-05', 253);
+insert into runs(type, description, km, run_date, training_id) values ('futás', 'indulás:4:38 Halesz:18p Kisfalud:58p M7:1ó09p Lótusz:1ó23p kanyarbüfé:1ó36p Agárd:2ó06p Velencefürdő:2ó34p Velence:2ó56p Spar:26p Katonai:8:43 Kisfalud:9:45 Óra:10:15', 53, '2025-09-13', 254);
+insert into runs(type, description, km, run_date, training_id) values ('futás', 'indulás:4:33 Agárd:2ó13p Velence:3ó07p Óra:10:30', 50, '2025-09-20', 255);
+insert into runs(type, description, km, run_date, training_id) values ('futás', 'indulás:5:00 Agárd:2ó10p Velence:3ó07p Óra:10:46', 55, '2025-09-27', 256);
 
 
 

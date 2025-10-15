bff5c4a0b047adfe0eb1c74386d3a7784933a170
hadikp <hp20001025@gmail.com>
2025-10-15 18:30:51 +0200

    add new sql

--- spring-boot-ecommerce/src/main/resources/db/migration/V1__product_category.sql
+++ spring-boot-ecommerce/src/main/resources/db/migration/V1__product_category.sql
@@ -3,4 +3,7 @@ CREATE TABLE product_category (
   category_name VARCHAR(255)
 );
 
-INSERT INTO product_category (category_name) VALUES ('BOOKS');
+INSERT INTO product_category(category_name) VALUES ('Books');
+INSERT INTO product_category(category_name) VALUES ('Coffee Mugs');
+INSERT INTO product_category(category_name) VALUES ('Mouse Pads');
+INSERT INTO product_category(category_name) VALUES ('Luggage Tags');

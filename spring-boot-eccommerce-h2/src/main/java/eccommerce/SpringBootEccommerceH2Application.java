dc1148656e845ee6712eda23640e815864037a4b
hadikp <hp20001025@gmail.com>
2025-10-30 16:49:48 +0100

    start projekt

---
+++ spring-boot-eccommerce-h2/src/main/java/eccommerce/SpringBootEccommerceH2Application.java
@@ -0,0 +1,13 @@
+package eccommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEccommerceH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEccommerceH2Application.class, args);
+	}
+
+}
---
+++ spring-boot-eccommerce-h2/src/test/java/eccommerce/SpringBootEccommerceH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package eccommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEccommerceH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
